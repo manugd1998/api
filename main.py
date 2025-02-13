432c2d0bd1aba33d67442ee4532bab8eb5e4afd8
manugd1998 <manugd1998@gmail.com>
2025-02-13 16:51:21 +0100

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from flask import Flask, jsonify, request
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-estado_solenoide = {"solenoide": "OFF"}  # Estado inicial
-
-@app.route('/estado-solenoide', methods=['GET'])
-def obtener_estado():
-    return jsonify(estado_solenoide)
-
-@app.route('/cambiar-solenoide', methods=['POST'])
-def cambiar_estado():
-    global estado_solenoide
-    datos = request.get_json()
-    if "solenoide" in datos:
-        estado_solenoide["solenoide"] = datos["solenoide"]
-        return jsonify({"mensaje": "Estado actualizado"}), 200
-    return jsonify({"error": "Parámetro faltante"}), 400
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.get("/")
+def read_root():
+    return {"message": "API funcionando correctamente"}
